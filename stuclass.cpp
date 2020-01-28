96f5f2cecabe5840e793b1c950e50237fbc3a519
muditarya <muduarya@gmail.com>
2020-01-28 16:47:28 +0530

    created class

---
+++ stuclass.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class myclass
+{
+public:
+	int mynum;
+	string mystring;
+}
+;
+int main()
+{
+	myclass myobject;
+	myobj.mynum = 1;
+	myobj.mystring="class";
+	cout << myobj.mynum << "\n";
+	cout << myobj.mystring;
+	return 0;
+}
